c71da953f4b31bdb0ebeac08f66d175914784039
Henrik Heinonen <hohein@utu.fi>
2025-02-06 11:27:09 +0200

    changes to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,11 +5,6 @@ const Footer = () => {
       <div className="footer-content">
         <p >Made with 🍪 by heinohen</p>
         <p>model trained by me, from utu comp vis course assignment</p>
-        <div >
-          <a href="https://github.com/heinohen" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        </div>
       </div>
     </footer>
   );
